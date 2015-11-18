f4c406c4e96984587526a2343dc292e2d6726ea9
mitchell486 <mitchell.bumgarner@gmail.com>
2015-11-17 22:51:35 -0600

    Created a git status script. -MB

---
+++ git_update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# search in my user directory. should be the only one with
+# any .git folders that need to have the status checked.
+
+# damnit OnlineCop, you're too damn good.
+results=$(find . -name \*.git 2>/dev/null | grep -oP '^(.*)(?=\/\.git$)' | while read line ; do pushd "$line"; git status; printf "\n"; popd; printf "\n"; done)
+
+echo "
+
+Your current git status is:
+----------------------------
+
+$results
+
+
+Thanks!
+"
